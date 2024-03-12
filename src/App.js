43c6806d3100870b498ae53b919526605760fa95
Serena Siliberti <serena.siliberti@gmail.com>
2024-03-12 16:37:09 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import AllTheBooks from "./components/AllTheBooks";
+import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome />
+      <MyNav />
+      <AllTheBooks />
+      <MyFooter />
     </div>
   );
 }
